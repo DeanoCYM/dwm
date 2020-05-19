d42af64f9b340956363395c0871bcf76970340b5
Ellis Rhys Thomas <e.rhys.thomas@gmail.com>
2020-05-19 11:09:10 +0300

    changed font to avoid Inconsolata glyphspacing bug, window rules

--- config.h
+++ config.h
@@ -5,8 +5,8 @@ static const unsigned int borderpx  = 5;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Inconsolata-Regular:size=16" };
-static const char dmenufont[]       = "Inconsolata-Regular:size=16";
+static const char *fonts[]          = { "Roboto Mono Regular:size=18" };
+static const char dmenufont[]       = "Roboto Mono Regular:size=18";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -27,10 +27,9 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class, instance, title                           tags    float monitor */
-	{ "Gimp", NULL, NULL,                               1,      0,    -1 },
+	{ NULL, NULL, "Default - Wine desktop",             0,      1     -1 },
 	{ NULL, NULL, "XBindKey: Hit a key",                0,      1,    -1 }, /* xbindkeys -k */
 	{ NULL, NULL, "Event Tester",                       0,      1,    -1 }, /* xev */
-	{ "Firefox",  NULL, NULL,                           1 << 2, 0,    -1 },
 	{ "Gnome-calculator", NULL, NULL,                   0,      1,    -1 },
 };
 
